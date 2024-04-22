4d25d607fd85ba657b082604d0799a47ab233b32
Kgoshi <ST10438922@imconnect.edu.za>
2024-04-22 11:48:32 +0200

    Created second screen (activity_play_screen.xml) and a button to take you from the activity_welcome.xml to the activity_play_screen.xml
    A welcome image has also been added, see: image_dogwelcomepage.jpg

---
+++ app/src/main/java/com/example/tomagatchidogapp/Play_Screen.kt
@@ -0,0 +1,14 @@
+package com.example.tomagatchidogapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Play_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_screen)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tomagatchidogapp/Welcome_Screen.kt
@@ -0,0 +1,25 @@
+package com.example.tomagatchidogapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Welcome_Screen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        var WelcomeButton =  findViewById<Button>(R.id.btnWelcome)
+
+        WelcomeButton?.setOnClickListener {
+            //creating an intent for the play screen page
+            var intent = Intent(this, Play_Screen::class.java)
+            //starting the activity
+            startActivity(intent)
+        }
+
+    }
+}
