33378be8f161355e7849c6f969d4ce9b49ceac32
Kgoshi <ST10438922@imconnect.edu.za>
2024-04-25 10:23:21 +0200

    Added the three progress bars to show the status of the hunger, cleanliness and happiness meters for the virtual pet.
    Implemented all the functionality of the three buttons to reflect what they will do during run time, this includes updating the progress bars accordingly and updating the picture of the virtual pet.

--- app/src/main/java/com/example/tomagatchidogapp/Welcome_Screen.kt
+++ app/src/main/java/com/example/tomagatchidogapp/Welcome_Screen.kt
@@ -5,13 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class Welcome_Screen : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        /*var imgWelcome = findViewById<ImageView>(R.id.imgWelcome)
 
+        imgWelcome.setBackgroundResource(R.drawable.image_dogwelcomepage)*/
         var WelcomeButton =  findViewById<Button>(R.id.btnWelcome)
 
         WelcomeButton?.setOnClickListener {
